3b4a61bb264c126ca31fc5d3b23ed54e5ec97c61
주희 윤 <s2014@e-mirim.hs.kr>
2021-09-14 09:06:43 +0900

    012345를 3번 반복하는 이중반복문

---
+++ C/banbok.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    
+    for (int i = 1; i <= 3; i++) {
+
+        for (int i = 0; i <= 5; i++) {
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+}
