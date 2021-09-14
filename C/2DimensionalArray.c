fd1cbbf03e4492940f3295bde37db0cf85900e94
주희 윤 <s2014@e-mirim.hs.kr>
2021-09-14 11:01:31 +0900

    2차원 배열

---
+++ C/2DimensionalArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    
+    int tarr[3][4] = {
+    { 1,2,3,4 },
+    { 5,6,7,8 },
+    { 9,10,11,12 } };
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", tarr[i][j]);
+        }
+        printf("\n");
+    }
+}
